4723b5f924e6bbbb3fdea7924919474f2511b205
CM Lubinski <cmc333333@gmail.com>
2010-12-12 00:06:46 -0600

    form -> shape

--- apitest/shape.py
+++ apitest/shape.py
@@ -1,18 +1,18 @@
 import unittest
 from common import Server
 
-class TestForm(unittest.TestCase):
+class TestShape(unittest.TestCase):
   def setUp(self):
     self.proxy = Server()
   def test_post(self):
-    (success, body) = self.proxy.post("form", {})
+    (success, body) = self.proxy.post("shape", {})
     self.assertFalse(success)
   def test_retrieve(self):
-    (success, body) = self.proxy.put("form/person", {"first" : {"type": "string"}, "last": {"type": "string"}})
+    (success, body) = self.proxy.put("shape/person", {"first" : {"type": "string"}, "last": {"type": "string"}})
     self.assertTrue(success)
-    (success, body) = self.proxy.put("form/pet", {"name": {"type": "string"}})
+    (success, body) = self.proxy.put("shape/pet", {"name": {"type": "string"}})
     self.assertTrue(success)
-    (success, body) = self.proxy.get("form")
+    (success, body) = self.proxy.get("shape")
     self.assertTrue(success)
     self.assertTrue("person" in body)
     self.assertTrue("first" in body["person"])
@@ -26,48 +26,48 @@ class TestForm(unittest.TestCase):
     self.assertTrue("type" in body["pet"]["name"])
     self.assertEqual(body["pet"]["name"]["type"], "string")
 
-    (success, body) = self.proxy.delete("form/person")
+    (success, body) = self.proxy.delete("shape/person")
     self.assertTrue(success)
-    (success, body) = self.proxy.delete("form/pet")
+    (success, body) = self.proxy.delete("shape/pet")
     self.assertTrue(success)
   def test_update(self):
-    (success, body) = self.proxy.put("form", {})
+    (success, body) = self.proxy.put("shape", {})
     self.assertFalse(success)
   def test_delete(self):
-    (success, body) = self.proxy.delete("form")
+    (success, body) = self.proxy.delete("shape")
     self.assertFalse(success)
 
-class TestFormElement(unittest.TestCase):
+class TestShapeElement(unittest.TestCase):
   def setUp(self):
     self.proxy = Server()
   def test_put_no_body(self):
     """Creation with no body should return an error"""
-    (success, body) = self.proxy.put("form/person")
+    (success, body) = self.proxy.put("shape/person")
     self.assertFalse(success)
-    (success, body) = self.proxy.put("form/person", {})
+    (success, body) = self.proxy.put("shape/person", {})
     self.assertFalse(success)
   def test_put_bad_fields(self):
     """Creation with bad fields"""
-    (success, body) = self.proxy.put("form/person", {"name": "something"})
+    (success, body) = self.proxy.put("shape/person", {"name": "something"})
     self.assertFalse(success)
-    (success, body) = self.proxy.put("form/person", {"name": {"type": "string"}, "other": 2})
+    (success, body) = self.proxy.put("shape/person", {"name": {"type": "string"}, "other": 2})
     self.assertFalse(success)
   def test_put(self):
     """Successful creation"""
-    (success, body) = self.proxy.put("form/person", {"first": {"type": "string"}, "last": {"type": "string"}})
+    (success, body) = self.proxy.put("shape/person", {"first": {"type": "string"}, "last": {"type": "string"}})
     self.assertTrue(success)
 
-    (success, body) = self.proxy.delete("form/person")
+    (success, body) = self.proxy.delete("shape/person")
     self.assertTrue(success)
   def test_retrieve_bad(self):
     """Retrieval of bad content is a 404"""
-    (success, body) = self.proxy.get("form/asdsdsafdf")
+    (success, body) = self.proxy.get("shape/asdsdsafdf")
     self.assertFalse(success)
   def test_retrieve(self):
     """Retrieval works"""
-    (success, body) = self.proxy.put("form/person", {"first": {"type": "string"}, "last": {"type": "string"}})
+    (success, body) = self.proxy.put("shape/person", {"first": {"type": "string"}, "last": {"type": "string"}})
     self.assertTrue(success)
-    (success, body) = self.proxy.get("form/person")
+    (success, body) = self.proxy.get("shape/person")
     self.assertTrue(success)
     self.assertTrue('first' in body)
     self.assertTrue('type' in body['first'])
@@ -76,72 +76,72 @@ class TestFormElement(unittest.TestCase):
     self.assertTrue('type' in body['last'])
     self.assertEqual(body['last']['type'], 'string')
 
-    (success, body) = self.proxy.delete("form/person")
+    (success, body) = self.proxy.delete("shape/person")
     self.assertTrue(success)
   def test_delete_bad(self):
     """Delete of bad content is a 404"""
-    (success, body) = self.proxy.delete("form/asdsdsafdf")
+    (success, body) = self.proxy.delete("shape/asdsdsafdf")
     self.assertFalse(success)
   def test_delete(self):
     """Correct use of delete"""
-    (success, body) = self.proxy.put("form/person", {"first": {"type": "string"}, "last": {"type": "string"}})
+    (success, body) = self.proxy.put("shape/person", {"first": {"type": "string"}, "last": {"type": "string"}})
     self.assertTrue(success)
-    (success, body) = self.proxy.get("form/person")
+    (success, body) = self.proxy.get("shape/person")
     self.assertTrue(success)
 
-    (success, body) = self.proxy.delete("form/person")
+    (success, body) = self.proxy.delete("shape/person")
     self.assertTrue(success)
-    (success, body) = self.proxy.get("form/person")
+    (success, body) = self.proxy.get("shape/person")
     self.assertFalse(success)
 
-class TestFormSubelement(unittest.TestCase):
+class TestShapeSubelement(unittest.TestCase):
   def setUp(self):
     self.proxy = Server()
-    (success, body) = self.proxy.put("form/pet", {"name": {"type": "string"}})
+    (success, body) = self.proxy.put("shape/pet", {"name": {"type": "string"}})
     self.assertTrue(success)
   def tearDown(self):
-    (success, body) = self.proxy.delete("form/pet")
+    (success, body) = self.proxy.delete("shape/pet")
     self.assertTrue(success)
   def test_put_no_body(self):
     """Creation with no body should return an error"""
-    (success, body) = self.proxy.put("form/pet/color")
+    (success, body) = self.proxy.put("shape/pet/color")
     self.assertFalse(success)
-    (success, body) = self.proxy.put("form/pet/color", {})
+    (success, body) = self.proxy.put("shape/pet/color", {})
     self.assertFalse(success)
   def test_put_bad_fields(self):
     """Creation with bad fields"""
-    (success, body) = self.proxy.put("form/pet/color", {"type": "something"})
+    (success, body) = self.proxy.put("shape/pet/color", {"type": "something"})
     self.assertFalse(success)
-    (success, body) = self.proxy.put("form/pet/color", {"type": 1})
+    (success, body) = self.proxy.put("shape/pet/color", {"type": 1})
     self.assertFalse(success)
   def test_put(self):
     """Successful creation"""
-    (success, body) = self.proxy.put("form/pet/color", {"type": "string"})
+    (success, body) = self.proxy.put("shape/pet/color", {"type": "string"})
     self.assertTrue(success)
   def test_retrieve_bad(self):
     """Retrieval of bad content is a 404"""
-    (success, body) = self.proxy.get("form/pet/asdsdsafdf")
+    (success, body) = self.proxy.get("shape/pet/asdsdsafdf")
     self.assertFalse(success)
   def test_retrieve(self):
     """Retrieval works"""
-    (success, body) = self.proxy.get("form/pet/name")
+    (success, body) = self.proxy.get("shape/pet/name")
     self.assertTrue(success)
     self.assertTrue('type' in body)
     self.assertEqual(body['type'], 'string')
   def test_delete_bad(self):
     """Delete of bad content is a 404"""
-    (success, body) = self.proxy.delete("form/pet/asdsdsafdf")
+    (success, body) = self.proxy.delete("shape/pet/asdsdsafdf")
     self.assertFalse(success)
   def test_delete(self):
     """Correct use of delete"""
-    (success, body) = self.proxy.put("form/pet/color", {"type": "string"})
+    (success, body) = self.proxy.put("shape/pet/color", {"type": "string"})
     self.assertTrue(success)
-    (success, body) = self.proxy.get("form/pet/color")
+    (success, body) = self.proxy.get("shape/pet/color")
     self.assertTrue(success)
 
-    (success, body) = self.proxy.delete("form/pet/color")
+    (success, body) = self.proxy.delete("shape/pet/color")
     self.assertTrue(success)
-    (success, body) = self.proxy.get("form/pet/color")
+    (success, body) = self.proxy.get("shape/pet/color")
     self.assertFalse(success)
 if __name__ == '__main__':
   unittest.main()
--- handlers/shape.py
+++ handlers/shape.py
@@ -4,35 +4,35 @@ from handlers import *
 from model import *
 import logging
 
-class Form(Handler):
+class Shape(Handler):
   #override
   def jsonGet(self, element, subelement):
     results = {}
     offset = 0
-    query = FormField.all().order('form')
+    query = ShapeField.all().order('shape')
     count = query.count()
     while offset < count:
       for field in query.fetch(20, offset):
         offset = offset + 1
-        if not field.form in results:
-          results[field.form] = {}
-        results[field.form][field.name] = {'type': field.fieldType, 'required': field.required}
+        if not field.shape in results:
+          results[field.shape] = {}
+        results[field.shape][field.name] = {'type': field.fieldType, 'required': field.required}
     self.success(results)
 
-class FormElement(Handler):
+class ShapeElement(Handler):
   #override
-  def jsonGet(self, form, subelement):
-    query = FormField.all().filter('form =', form)
+  def jsonGet(self, shape, subelement):
+    query = ShapeField.all().filter('shape =', shape)
     results = {}
-    for field in query.fetch(50): # Each form may have at most 50 fields
+    for field in query.fetch(50): # Each shape may have at most 50 fields
       results[field.name] = {'type': field.fieldType, 'required': field.required}
     if len(results) == 0:
       self.error(404)
     else:
       self.success(results)
   #override
-  def jsonDelete(self, form, subelement):
-    query = FormField.all().filter('form = ', form)
+  def jsonDelete(self, shape, subelement):
+    query = ShapeField.all().filter('shape = ', shape)
     if query.count() == 0:
       self.error(404)
     else:
@@ -40,12 +40,12 @@ class FormElement(Handler):
         field.delete()
       self.success()
   #override
-  def jsonPut(self, body, form, subelement):
+  def jsonPut(self, body, shape, subelement):
     fields = body.keys()
     if len(fields) == 0:
       self.fail('must have at least one field')
     elif len(fields) > 50:
-      self.fail('each form may have at most 50 fields')
+      self.fail('each shape may have at most 50 fields')
     else:
       # verify that every field has a type and that said type is valid
       valid = True
@@ -54,13 +54,13 @@ class FormElement(Handler):
           valid = False
           self.fail(field + ' does not have a type')
           break
-        if body[field]['type'] not in FormFieldTypes:
+        if body[field]['type'] not in ShapeFieldTypes:
           valid = False
           self.fail(str(body[field]['type']) + ' is an invalid type')
           break
 
       if valid:
-        existing = FormField.all().filter('form =', form).fetch(50)
+        existing = ShapeField.all().filter('shape =', shape).fetch(50)
         new = body.keys()
         for field in existing:
           if field.name in fields: # Modifying
@@ -70,14 +70,14 @@ class FormElement(Handler):
             field.delete()
         # Create the new ones
         for field in new:
-          FormField(form=form, name=field, fieldType=body[field]['type']).put()
+          ShapeField(shape=shape, name=field, fieldType=body[field]['type']).put()
 
         self.success()
 
-class FormSubelement(Handler):
+class ShapeSubelement(Handler):
   #override
-  def jsonGet(self, form, field):
-    query = FormField.all().filter('form =', form).filter('name =', field)
+  def jsonGet(self, shape, field):
+    query = ShapeField.all().filter('shape =', shape).filter('name =', field)
     if query.count() == 0:
       self.error(404)
     else:
@@ -85,33 +85,33 @@ class FormSubelement(Handler):
       result = {'type': field.fieldType, 'required': field.required}
       self.success(result)
   #override
-  def jsonDelete(self, form, field):
-    query = FormField.all().filter('form = ', form).filter('name =', field)
+  def jsonDelete(self, shape, field):
+    query = ShapeField.all().filter('shape = ', shape).filter('name =', field)
     if query.count() == 0:
       self.error(404)
     else:
       query.fetch(1)[0].delete()
       self.success()
   #override
-  def jsonPut(self, body, form, field):
+  def jsonPut(self, body, shape, field):
     if 'type' not in body:
       valid = False
       self.fail(field + ' does not have a type')
-    elif body['type'] not in FormFieldTypes:
+    elif body['type'] not in ShapeFieldTypes:
       valid = False
       self.fail(str(body['type']) + ' is an invalid type')
     else:
-      existing = FormField.all().filter('form =', form).filter('name =', field).fetch(1)
+      existing = ShapeField.all().filter('shape =', shape).filter('name =', field).fetch(1)
       if len(existing) > 0:
         existing[0].updateType(body['type']).put()
       else:
-        FormField(form=form, name=field, fieldType=body['type']).put()
+        ShapeField(shape=shape, name=field, fieldType=body['type']).put()
       self.success()
 
 application = webapp.WSGIApplication([
-  ('/form', Form),
-  (r'/form/(.*)/(.*)', FormSubelement),
-  (r'/form/(.*)', FormElement)
+  ('/shape', Shape),
+  (r'/shape/(.*)/(.*)', ShapeSubelement),
+  (r'/shape/(.*)', ShapeElement)
   ], debug=True)
 
 def main():
